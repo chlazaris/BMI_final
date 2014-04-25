4fd253712c4928d577ddb8b8a3ad9a0abd7aaed2
Harris A. Lazaris <lazaris@nyu.edu>
2014-04-24 20:00:47 -0400

    New boxplot script that puts the plots in the right order and creates visible legends

--- Boxplots/boxplot.py
+++ Boxplots/boxplot.py
@@ -1,12 +1,22 @@
+#!/bin/python 
 
+# import necessary modules
 import pandas
 import matplotlib as mpl
 from matplotlib import pyplot
+import sys
 
-data = pandas.read_table('Hind3_NcoI_Ming_total_boxplot_final_input.txt', header=None)
+# Get input file as argument
+input_file = sys.argv[1]
+
+data = pandas.read_table(input_file, header=None)
 data.columns=['chrom','corr','sample']
 
-output = pandas.DataFrame.boxplot(data, column='corr', by='sample')
+# sample = ['HindIII_1_2_flt','NcoI_1_2_flt','HindIII_NcoI_1_flt','HindIII_NcoI_2_flt',
+# 'HindIII_1_2_cor','NcoI_1_2_cor','HindIII_NcoI_1_cor','HindIII_NcoI_2_cor']
+
+output = pandas.DataFrame.boxplot(data, column='corr', by='sample', rot=45, fontsize=6, grid=False)
 
 fig = output.get_figure()
-fig.savefig('boxplot.pdf')
+out_file = input_file.strip("_input.txt") + ".pdf"
+fig.savefig(out_file)
